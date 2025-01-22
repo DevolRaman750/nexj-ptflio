fe876721134d54675e5e73a6440e55039f8240f6
RamanT <149133866+DevolRaman750@users.noreply.github.com>
2025-01-22 16:58:46 +0530

    After reference error

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
-
+'use client'
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Main from "@/components/Main";
 import RecentProjects from "@/components/RecentProjects";
 import {  FloatingNavbar } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import { useEffect } from "react";
 
 
 
 export default function Home() {
+  useEffect(() => {
+    // This code will only run on the client side
+    if (typeof document !== 'undefined') {
+      // Your client-side code here
+    }
+  }, []);
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto overflow-clip sm:px-10 px-5">
       <div className="max-w-7xl w-full">
