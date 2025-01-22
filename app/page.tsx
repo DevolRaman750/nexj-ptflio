eb4fa8c72530619c13ae0f6e9ea9687976a322f7
RamanT <149133866+DevolRaman750@users.noreply.github.com>
2025-01-22 17:18:55 +0530

    After reference error 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Main from "@/components/Main";
 import RecentProjects from "@/components/RecentProjects";
-import {  FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import { useEffect } from "react";
-
-
 
 export default function Home() {
   useEffect(() => {
@@ -20,15 +18,11 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto overflow-clip sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNavbar NavItems={
-          navItems
-        }
-
-        />
-        <Main/>
-        <Grid/>
-        <RecentProjects/>
-        <Footer/>
+        <FloatingNavbar NavItems={navItems} />
+        <Main />
+        <Grid />
+        <RecentProjects />
+        <Footer />
       </div>
     </main>
   );
